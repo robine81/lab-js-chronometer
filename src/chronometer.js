998b4c29f82f4e2f49b0ef650994dc711a0ca65a
robine81 <robine.westberg@gmail.com>
2023-04-04 14:47:00 +0200

    first iteration done, will contnue tonight

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,10 +1,24 @@
 class Chronometer {
   constructor() {
+    //increment by one by each second
+    this.currentTime = 0 
+    this.intervalId = null
     // ... your code goes here
   }
 
+  //function when start button is pressed. prints the time to the page
   start(printTimeCallback) {
-    // ... your code goes here
+    // if(!printTimeCallback) return
+    
+    
+    this.intervalId = setInterval(()=>{
+      
+    this.currentTime+=1
+    if(printTimeCallback){
+     
+      printTimeCallback()
+    }
+      }, 1000)    
   }
 
   getMinutes() {
